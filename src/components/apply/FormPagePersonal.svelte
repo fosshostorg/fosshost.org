71a1a5085cb10b85c4ef0e836a06aefa939e07f3
knightss@catlin.edu <knightss@catlin.edu>
2021-03-15 22:38:55 -0700

    Start of apply form.

--- src/components/apply/FormPagePersonal.svelte
+++ src/components/apply/FormPagePersonal.svelte
@@ -1,10 +1,14 @@
-<script>
-
+<script lang="ts">
+    import Input from './Input.svelte';
+    export let data: FormResponse;
 </script>
 
-<h1>
-    Personal
-</h1>
+<main>
+    <h1>Personal Information</h1>
+    <Input label="Full name" type="text" placeholder="What's your name" bind:value={data.personal.name} />
+    <Input label="Email" type="email" placeholder="What's your name" bind:value={data.personal.email} />
+</main>
+
 
 <style>
 
---
+++ src/components/apply/Input.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let value: string = "";
+    export let label: string = "Label";
+    export let placeholder: string = "Enter your text...";
+    export let name: string = "input";
+
+</script>
+
+<label>
+    {label}
+    <input bind:value {name} {placeholder} {...$$restProps} />
+</label>
+
+<style>
+    label {
+        display: flex;
+        flex-direction: column;
+        color: var(--fosshost-black);
+        font-size: 18px;
+        line-height: 24px;
+    }
+
+    input {
+        font-size: 18px;
+        line-height: 24px;
+        padding: 0.5rem;
+        border: 2px solid var(--fosshost-light-grey);
+        border-radius: 4px;
+        margin-top: 0.25rem;
+    }
+
+</style>
