835524520117838f00bed59232268bc12434ead8
Seth Knights <knightss@catlin.edu>
2021-03-17 23:40:46 -0700

    Fix fonts on nav.

--- src/components/apply/FormPageTechnical.svelte
+++ src/components/apply/FormPageTechnical.svelte
@@ -1,11 +1,19 @@
-<script>
-
+<script lang="ts">
+    import Input from './Input.svelte';
+    export let data: FormResponse;
+    export let currentPage: number;
 </script>
 
-<h1>
-    Technical
-</h1>
+<main>
+    <h1>Technical Information</h1>
+    <p>We’d like to know more about the project you are working on. Please specify the necessary details below.</p>
+    <div class="buttons">
+        <button class="back" on:click={() => {currentPage -= 1}}>Back</button>
+        <button class="submit" on:click={() => {currentPage += 1}}>Next</button>
+    </div>
+</main>
+
 
 <style>
-
+    
 </style>
