c3a59b83c479456538e2e00b7693ec62c06df4ee
Seth Knights <knightss@catlin.edu>
2021-03-18 00:37:34 -0700

    New checkbox group component added.

---
+++ src/components/apply/CheckboxGroup.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    export let options: any[];
+    export let selected: any[];
+</script>
+
+<div>
+{#each options as option}
+    <label class:checked={selected.includes(option)}>
+        <input type="checkbox" bind:group={selected} value={option}/>
+        {option}
+    </label>
+{/each}
+</div>
+
+<style>
+    div {
+        width: auto;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+    }
+
+    input {
+        display: none;
+    }
+
+    label {
+        height: 24px;
+        padding-left: 2.3rem;
+        display: flex;
+        position: relative;
+        align-items: center;
+        font-size: 18px;
+        line-height: 24px;
+        color: var(--fosshost-grey);
+        margin: 0 1rem 1rem;
+        cursor: pointer;
+    }
+
+    label:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 20px;
+        height: 20px;
+        border: 2px solid var(--fosshost-light-grey);
+        border-radius: 2px;
+    }
+
+    label.checked:before {
+        background-color: var(--fosshost-light-grey);
+    }
+
+    label.checked:after {
+        content: '';
+        width: 12px;
+        height: 4px;
+        position: absolute;
+        top: 10px;
+        left: 6px;
+        background-color: white;
+    }
+
+</style>
--- src/components/apply/FormPageTechnical.svelte
+++ src/components/apply/FormPageTechnical.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+    import CheckboxGroup from './CheckboxGroup.svelte';
     import Input from './Input.svelte';
     export let data: FormResponse;
     export let currentPage: number;
+
+    let options = [
+        "X86 VPS",
+        "Mirrors-as-a-service",
+        "AArch64 VPS",
+        "Email and Webhosting",
+        "DNS",
+        "Audio and Video Conferencing",
+    ]
+
 </script>
 
 <main>
     <h1>Technical Information</h1>
     <p>We’d like to know more about the project you are working on. Please specify the necessary details below.</p>
+    <CheckboxGroup {options} bind:selected={data.technical.services} />
     <div class="buttons">
         <button class="back" on:click={() => {currentPage -= 1}}>Back</button>
         <button class="submit" on:click={() => {currentPage += 1}}>Next</button>
@@ -15,5 +27,5 @@
 
 
 <style>
-    
+
 </style>
