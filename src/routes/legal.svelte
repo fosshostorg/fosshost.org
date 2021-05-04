d7fa393959485b28c27e62b65b6c525bbb611fd1
Seth Knights <knightss@catlin.edu>
2021-05-03 22:29:45 -0700

    Add legal page.

---
+++ src/routes/legal.svelte
@@ -0,0 +1,27 @@
+<script>
+  import marked from "marked";
+  import text from "../static_pages/legal.md";
+</script>
+
+<svelte:head>
+  <title>Fosshost - Legal</title>
+</svelte:head>
+
+<main>
+  <article>
+    {@html marked(text)}
+  </article>
+</main>
+
+<style>
+  main {
+    width: 95%;
+    max-width: 780px;
+    margin: 3rem auto;
+  }
+  @media (max-width: 1280px) {
+    main {
+      margin: 0.5rem auto 3rem;
+    }
+  }
+</style>
