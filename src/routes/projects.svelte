4c9036cad053e4f77030f810ccb15f033afbd628
Seth Knights <knightss@catlin.edu>
2021-05-07 22:52:23 -0700

    Sanitize input before creating issue.

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -1,24 +1,24 @@
 <script>
-    import Header from "../components/Header.svelte";
-    import ProjectList from "../components/projects/ProjectList.svelte";
+  import Header from "../components/Header.svelte";
+  import ProjectList from "../components/projects/ProjectList.svelte";
 </script>
 
 <svelte:head>
-	<title>Fosshost - Projects</title>
+  <title>Fosshost - Projects</title>
 </svelte:head>
 
 <main>
-    <Header 
-        title="We provide a tailored hosting service to fit your needs"
-        subtitle="We provide a maintained list of projects that benefit from our service. Some of these projects may have hundreds of sub-projects that we do not list here."
-        src="/img/illustration0.png"
-    />
-    <ProjectList />
-    <!-- TODO: Pass data through the list here? -->
+  <Header
+    title="We provide a tailored hosting service to fit your needs"
+    subtitle="We provide a maintained list of projects that benefit from our service. Some of these projects may have hundreds of sub-projects that we do not list here."
+    src="/img/illustration0.png"
+  />
+  <ProjectList />
+  <!-- TODO: Pass data through the list here? -->
 </main>
 
 <style>
-    main {
-        width: 100%;
-    }
+  main {
+    width: 100%;
+  }
 </style>
