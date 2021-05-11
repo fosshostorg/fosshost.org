d68d452e4494c4831c6ecfdfa1a2489e62d31367
Seth Knights <knightss@catlin.edu>
2021-05-10 20:12:02 -0700

    Add new projects.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -32,8 +32,15 @@ export const Projects: Project[] = [
         description: "",
     },
     {
+        id: 'xfce',
+        img: '/img/project-logos/xfce.svg',
+        name: 'XFCE',
+        link: 'https://xfce.org/',
+        description: ''
+    },
+    {
         id: "xubuntu",
-        img: "/img/project-logos/xubuntu.svg",
+        img: "/img/project-logos/xubuntu.png",
         name: "Xubuntu",
         link: "https://xubuntu.org",
         description: "",
@@ -46,17 +53,640 @@ export const Projects: Project[] = [
         description: "",
     },
     {
-        id: "test",
-        img: "",
-        name: "TestProject",
-        link: "https://fosshost.org",
-        description: "",
-    },
-    {
         id: "fsfe",
         img: "",
         name: "Free Software Foundation Europe",
         link: "https://fsfe.org/",
         description: "",
+    },
+    {
+      id: 'manjaro linux',
+      img: '',
+      name: 'Manjaro Linux',
+      link: 'https://manjaro.org/',
+      description: ''
+    },
+    {
+      id: 'celestia space',
+      img: '',
+      name: 'Celestia Space',
+      link: 'https://celestia.space/',
+      description: ''
+    },
+    {
+      id: 'serpent os',
+      img: '',
+      name: 'Serpent OS',
+      link: 'https://serpentos.com/',
+      description: ''
+    },
+    {
+      id: 'xfce nation',
+      img: '',
+      name: 'XFCE Nation',
+      link: 'https://xfcenation.com/',
+      description: ''
+    },
+    {
+      id: 'xiph',
+      img: '',
+      name: 'XIPH',
+      link: 'https://xiph.org/',
+      description: ''
+    },
+    {
+      id: 'activitypub conf',
+      img: '',
+      name: 'ActivityPub Conf',
+      link: 'https://conf.activitypub.rocks/',
+      description: ''
+    },
+    {
+      id: 'librespeed',
+      img: '',
+      name: 'LibreSpeed',
+      link: 'https://librespeed.org/',
+      description: ''
+    },
+    {
+      id: 'ubuntudde',
+      img: '',
+      name: 'UbuntuDDE',
+      link: 'https://ubuntudde.com/',
+      description: ''
+    },
+    {
+      id: 'cinelartv',
+      img: '',
+      name: 'CinelarTV',
+      link: 'https://github.com/CinelarTV/',
+      description: ''
+    },
+    {
+      id: 'freecad',
+      img: '',
+      name: 'FreeCAD',
+      link: 'https://www.freecadweb.org/',
+      description: ''
+    },
+    {
+      id: 'mirazheze bots',
+      img: '',
+      name: 'Mirazheze Bots',
+      link: 'https://mirahezeBots.org',
+      description: ''
+    },
+    {
+      id: 'community tech',
+      img: '',
+      name: 'Community Tech',
+      link: 'https://comm-tech.org/',
+      description: ''
+    },
+    {
+      id: 'portland linux user group',
+      img: '',
+      name: 'Portland Linux User Group',
+      link: 'https://www.pdxlinux.org/',
+      description: ''
+    },
+    {
+      id: 'dublin linux user group',
+      img: '',
+      name: 'Dublin Linux User Group',
+      link: 'https://dublinlinux.org/',
+      description: ''
+    },
+    {
+      id: 'ubuntu unity',
+      img: '',
+      name: 'Ubuntu Unity',
+      link: 'https://ubuntuunity.org/',
+      description: ''
+    },
+    {
+      id: 'entware',
+      img: '',
+      name: 'Entware',
+      link: 'https://entware.net/',
+      description: ''
+    },
+    {
+      id: 'sunengine',
+      img: '',
+      name: 'SunEngine',
+      link: 'https://github.com/sunengine/SunEngine/',
+      description: ''
+    },
+    {
+      id: 'drauger os',
+      img: '',
+      name: 'Drauger OS',
+      link: 'https://draugeros.org/',
+      description: ''
+    },
+    {
+      id: 'hosted status page',
+      img: '',
+      name: 'Hosted Status Page',
+      link: 'https://github.com/dragstor',
+      description: ''
+    },
+    {
+      id: 'web2pdf',
+      img: '',
+      name: 'Web2PDF',
+      link: 'https://github.com/warenix/Web2PDF/',
+      description: ''
+    },
+    {
+      id: 'fosshost mirrors',
+      img: '',
+      name: 'Fosshost Mirrors',
+      link: 'https://mirrors.fossho.st/',
+      description: ''
+    },
+    {
+      id: 'garuda linux',
+      img: '',
+      name: 'Garuda Linux',
+      link: 'https://garudalinux.in/',
+      description: ''
+    },
+    {
+      id: 'white spaces',
+      img: '',
+      name: 'White Spaces',
+      link: 'https://github.com/MishkinBerteig/white-spaces/',
+      description: ''
+    },
+    {
+      id: 'resistancecn',
+      img: '',
+      name: 'ResistanceCN',
+      link: 'https://github.com/ResistanceCN',
+      description: ''
+    },
+    {
+      id: 'genixcms',
+      img: '',
+      name: 'GeniXCMS',
+      link: 'https://github.com/semplon/GeniXCMS',
+      description: ''
+    },
+    {
+      id: 'dot hq',
+      img: '',
+      name: 'Dot HQ',
+      link: 'https://dothq.co/',
+      description: ''
+    },
+    {
+      id: 'webarch co-operative',
+      img: '',
+      name: 'Webarch Co-operative',
+      link: 'https://www.webarchitects.coop/',
+      description: ''
+    },
+    {
+      id: 'turbowarp',
+      img: '',
+      name: 'TurboWarp',
+      link: 'https://turbowarp.org/',
+      description: ''
+    },
+    {
+      id: 'dungeon-mode',
+      img: '',
+      name: 'Dungeon-mode',
+      link: 'https://github.com/mplsCorwin/dungeon-mode',
+      description: ''
+    },
+    {
+      id: 'ymir gnu linux',
+      img: '',
+      name: 'Ymir GNU Linux',
+      link: 'https://ymirlinux.org',
+      description: ''
+    },
+    {
+      id: 'linux lite os',
+      img: '',
+      name: 'Linux Lite OS',
+      link: 'https://linuxliteos.com/',
+      description: ''
+    },
+    {
+      id: 'desq',
+      img: '',
+      name: 'DesQ',
+      link: 'https://gitlab.com/DesQ/',
+      description: ''
+    },
+    {
+      id: 'freedesktop-sdk',
+      img: '',
+      name: 'FreeDesktop-SDK',
+      link: 'https://freedesktop-sdk.io/',
+      description: ''
+    },
+    {
+      id: 'dendrite',
+      img: '',
+      name: 'Dendrite',
+      link: 'https://github.com/matrix-org/dendrite',
+      description: ''
+    },
+    {
+      id: 'goadmin',
+      img: '',
+      name: 'GoAdmin',
+      link: 'https://github.com/GoAdminGroup/go-admin',
+      description: ''
+    },
+    {
+      id: 'kamuda os',
+      img: '',
+      name: 'Kamuda OS',
+      link: 'https://kumuda-os.com/',
+      description: ''
+    },
+    {
+      id: 'webmp',
+      img: '',
+      name: 'WebMP',
+      link: 'https://github.com/webcirque/webmp',
+      description: ''
+    },
+    {
+      id: 'graphene',
+      img: '',
+      name: 'Graphene',
+      link: 'https://github.com/oscarlab/graphene',
+      description: ''
+    },
+    {
+      id: 'archlinux qbot',
+      img: '',
+      name: 'ArchLinux QBot',
+      link: 'https://github.com/anna328p/qbot',
+      description: ''
+    },
+    {
+      id: 'free software foundation europe',
+      img: '',
+      name: 'Free Software Foundation Europe',
+      link: 'https://fsfe.org',
+      description: ''
+    },
+    {
+      id: 'f-droid',
+      img: '',
+      name: 'F-Droid',
+      link: 'https://f-droid.org/',
+      description: ''
+    },
+    {
+      id: 'whonix',
+      img: '',
+      name: 'Whonix',
+      link: 'https://www.whonix.org/',
+      description: ''
+    },
+    {
+      id: 'amarok linux',
+      img: '',
+      name: 'Amarok Linux',
+      link: 'https://amaroklinux.org',
+      description: ''
+    },
+    {
+      id: 'user repository',
+      img: '',
+      name: 'User Repository',
+      link: 'https://userrepository.eu',
+      description: ''
+    },
+    {
+      id: 'kde',
+      img: '',
+      name: 'KDE',
+      link: 'https://kde.org',
+      description: ''
+    },
+    {
+      id: 'kaos',
+      img: '',
+      name: 'KaOS',
+      link: 'https://kaosx.us',
+      description: ''
+    },
+    {
+      id: 'gnu guix',
+      img: '',
+      name: 'GNU Guix',
+      link: 'https://guix.gnu.org',
+      description: ''
+    },
+    {
+      id: 'join jabber',
+      img: '',
+      name: 'Join Jabber',
+      link: 'https://joinjabber.org/',
+      description: ''
+    },
+    {
+      id: 'ip fire',
+      img: '',
+      name: 'IP Fire',
+      link: 'https://www.ipfire.org/',
+      description: ''
+    },
+    {
+      id: 'internet archive',
+      img: '',
+      name: 'Internet Archive',
+      link: 'https://www.archive.org/',
+      description: ''
+    },
+    {
+      id: 'institute for computing in research',
+      img: '',
+      name: 'Institute for Computing in Research',
+      link: 'https://computinginresearch.org/',
+      description: ''
+    },
+    {
+      id: 'packetframe',
+      img: '',
+      name: 'Packetframe',
+      link: 'https://packetframe.com/',
+      description: ''
+    },
+    {
+      id: 'unofficial spacex community',
+      img: '',
+      name: 'Unofficial SpaceX Community',
+      link: 'https://spacex.sp.fyi/',
+      description: ''
+    },
+    {
+      id: 'skybase',
+      img: '',
+      name: 'Skybase',
+      link: 'https://skybasedb.com/',
+      description: ''
+    },
+    {
+      id: 'zoo-project',
+      img: '',
+      name: 'ZOO-Project',
+      link: 'http://zoo-project.org/',
+      description: ''
+    },
+    {
+      id: 'librematrix',
+      img: '',
+      name: 'LibreMatrix',
+      link: 'https://github.com/LibreMatrix/LibreMatrix',
+      description: ''
+    },
+    {
+      id: 'ersilia',
+      img: '',
+      name: 'Ersilia',
+      link: 'http://ersilia.io/',
+      description: ''
+    },
+    {
+      id: 'i make foss',
+      img: '',
+      name: 'I Make FOSS',
+      link: 'https://imakefoss.org/',
+      description: ''
+    },
+    {
+      id: 'macosicons',
+      img: '',
+      name: 'macOSicons',
+      link: 'https://macosicons.com/',
+      description: ''
+    },
+    {
+      id: 'chamilo',
+      img: '',
+      name: 'Chamilo',
+      link: 'https://chamilo.org/',
+      description: ''
+    },
+    {
+      id: 'gnu health',
+      img: '',
+      name: 'GNU Health',
+      link: 'https://www.gnuhealth.org/',
+      description: ''
+    },
+    {
+      id: 'emacsconf',
+      img: '',
+      name: 'EmacsConf',
+      link: 'https://emacsconf.org',
+      description: ''
+    },
+    {
+      id: 'blackarch',
+      img: '',
+      name: 'BlackArch',
+      link: 'https://github.com/blackarch',
+      description: ''
+    },
+    {
+      id: 'troglobit',
+      img: '',
+      name: 'Troglobit',
+      link: 'https://troglobit.com',
+      description: ''
+    },
+    {
+      id: 'mdwebscrapers',
+      img: '',
+      name: 'Mdwebscrapers',
+      link: 'http://github.com/biocrawler/md-web-scrapers',
+      description: ''
+    },
+    {
+      id: 'laxer os',
+      img: '',
+      name: 'Laxer OS',
+      link: 'https://www.laxeros.org/',
+      description: ''
+    },
+    {
+      id: 'aurora store',
+      img: '',
+      name: 'Aurora Store',
+      link: 'https://gitlab.com/AuroraOSS/AuroraStore',
+      description: ''
+    },
+    {
+      id: 'easybuild',
+      img: '',
+      name: 'EasyBuild',
+      link: 'https://easybuild.io',
+      description: ''
+    },
+    {
+      id: 'rebornos',
+      img: '',
+      name: 'RebornOS',
+      link: 'https://www.rebornos.org',
+      description: ''
+    },
+    {
+      id: 'fluffyscratch',
+      img: '',
+      name: 'FluffyScratch',
+      link: 'https://github.com/hamptonmoore/FluffyScratch',
+      description: ''
+    },
+    {
+      id: 'the network ups tools',
+      img: '',
+      name: 'The Network UPS Tools',
+      link: 'https://github.com/networkupstools/nut/',
+      description: ''
+    },
+    {
+      id: 'openindiana',
+      img: '',
+      name: 'OpenIndiana',
+      link: 'https://www.openindiana.org/',
+      description: ''
+    },
+    {
+      id: 'aleksis',
+      img: '',
+      name: 'AlekSIS',
+      link: 'https://aleksis.org',
+      description: ''
+    },
+    {
+      id: 'mellium',
+      img: '',
+      name: 'Mellium',
+      link: 'https://mellium.im',
+      description: ''
+    },
+    {
+      id: 'fosshost aarch64',
+      img: '',
+      name: 'Fosshost AArch64',
+      link: 'https://aarch64.com',
+      description: ''
+    },
+    {
+      id: 'cubocore',
+      img: '',
+      name: 'CuboCore',
+      link: 'https://cubocore.org',
+      description: ''
+    },
+    {
+      id: 'rocky linux',
+      img: '',
+      name: 'Rocky Linux',
+      link: 'https://rockylinux.org',
+      description: ''
+    },
+    {
+      id: 'arco linux',
+      img: '',
+      name: 'Arco Linux',
+      link: 'https://github.com/arcolinux',
+      description: ''
+    },
+    {
+      id: 'qemu',
+      img: '',
+      name: 'QEMU',
+      link: 'https://gitlab.com/qemu-project/qemu/',
+      description: ''
+    },
+    {
+      id: 'app manager',
+      img: '',
+      name: 'App Manager',
+      link: 'https://github.com/tahoe-lafs/tahoe-lafs',
+      description: ''
+    },
+    {
+      id: 'blog do edivaldo',
+      img: '',
+      name: 'Blog do Edivaldo',
+      link: 'https://edivaldobrito.com.br',
+      description: ''
+    },
+    {
+      id: 'generic linux',
+      img: '',
+      name: 'Generic Linux',
+      link: 'https://genericlinux.org',
+      description: ''
+    },
+    {
+      id: 'tahoe-lafs',
+      img: '',
+      name: 'Tahoe-LAFS',
+      link: 'https://github.com/tahoe-lafs/tahoe-lafs',
+      description: ''
+    },
+    {
+      id: 'pionir',
+      img: '',
+      name: 'Pionir',
+      link: 'https://github.com/pioniredu',
+      description: ''
+    },
+    {
+      id: 'orangemn6',
+      img: '',
+      name: 'OrangeMN6',
+      link: 'https://github.com/orangemn6',
+      description: ''
+    },
+    {
+      id: 'edudash',
+      img: '',
+      name: 'EduDash',
+      link: 'https://github.com/EduDash',
+      description: ''
+    },
+    {
+      id: 'sparkylinux',
+      img: '',
+      name: 'SparkyLinux',
+      link: 'https://sparkylinux.org',
+      description: ''
+    },
+    {
+      id: 'heptapod',
+      img: '',
+      name: 'HeptaPod',
+      link: 'https://heptapod.net',
+      description: ''
+    },
+    {
+      id: 'cob:web',
+      img: '',
+      name: 'Cob:web',
+      link: 'https://cob-web.xyz/',
+      description: ''
+    },
+    {
+      id: 'jitesoft - docker images',
+      img: '',
+      name: 'Jitesoft - Docker Images',
+      link: 'https://github.com/orgs/jitesoft/packages',
+      description: ''
     }
-]
+  ]
