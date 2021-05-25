c72788af656318b0fd903c7766effa3c52d551bd
Seth Knights <knightss@catlin.edu>
2021-05-25 08:04:42 -0700

    Little media page.

---
+++ src/routes/media.svelte
@@ -0,0 +1,29 @@
+<script>
+    import marked from "marked";
+    import text from "../static_pages/media.md";
+  </script>
+  
+  <svelte:head>
+    <title>Fosshost - Media</title>
+  </svelte:head>
+  
+  <main>
+    <article>
+      {@html marked(text)}
+    </article>
+  </main>
+  
+  <style>
+    main {
+      width: auto;
+      max-width: 780px;
+      margin: 3rem auto;
+    }
+    @media (max-width: 1280px) {
+      main {
+        margin: 0.5rem auto 3rem;
+        padding: 0rem 1rem;
+      }
+    }
+  </style>
+  
---
+++ src/static_pages/media.md
@@ -0,0 +1,4 @@
+# Media
+---
+
+You can download Fosshost media assets [here](https://fosshost.net/_nuxt/e2115e1a8cf61fd490ac8569d3305bb2.zip).
