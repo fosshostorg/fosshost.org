7192f0d5dc06d53786f2af80c806922d855bdf30
Seth Knights <knightss@catlin.edu>
2021-05-26 23:57:01 -0700

    Fix form formatting. Remove A/V.

--- src/components/apply/FormPagePersonal.svelte
+++ src/components/apply/FormPagePersonal.svelte
@@ -23,7 +23,7 @@
     <h1>Personal Information</h1>
     <p>Please note that we need your full name and contact detail in order to process your application.</p>
     <Input on:blur={() => {errors = validate(validator, 'name', data.personal, errors)}} error={errors.name} label="Full name" type="text" placeholder="What's your name" bind:value={data.personal.name} />
-    <Input on:blur={() => {errors = validate(validator, 'email', data.personal, errors)}} error={errors.email} label="Email" type="text" placeholder="you@company.com" bind:value={data.personal.email} />
+    <Input on:blur={() => {errors = validate(validator, 'email', data.personal, errors)}} error={errors.email} label="Email" type="email" placeholder="you@example.com" bind:value={data.personal.email} />
     <div class="buttons">
         <button class="submit" on:click={() => {errors = pageValidate(validator, data.personal, errors)}}>Next</button>
     </div>
