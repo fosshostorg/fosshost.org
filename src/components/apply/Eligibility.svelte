eb24cddf351f45456db7e4d8b5a9378a7418f33f
Seth Knights <knightss@catlin.edu>
2021-06-09 14:43:47 -0700

    Block new applications.

--- src/components/apply/Eligibility.svelte
+++ src/components/apply/Eligibility.svelte
@@ -8,16 +8,27 @@
         data.security.hasAcceptedCriteria = true;
         currentPage += 1;
     }
+
+    const formDisabled: boolean = true;
 </script>
 
 <main in:fade={{delay: 0, duration: 400}}>
-    <h1>Thank you for your interest in Fosshost</h1>
-    <p>
-        Before we proceed with your application, please confirm that you have read and understood the
-        <a href="https://docs.fosshost.org/en/home/application" target="_blank" class="fosshost-link"><b>Eligibility Criteria</b></a>.
-    </p>
-    <button class="accept" on:click={handleClick}>I accept</button>
-    <button class="cancel" on:click={() => {window.location.href = "/"}}>I don't accept</button>
+    {#if formDisabled}
+        <h1>Thank you for your interest in Fosshost</h1>
+        <p>
+            Unfortunately, applications for our services are temporarily suspended. To read more about the situation, click
+            <a href="/news/applications-temporarily-suspended" class="fosshost-link"><b>here</b></a>.
+        </p>
+        <button class="accept" on:click={() => {window.location.href = "/"}}>Back to home</button>
+    {:else}
+        <h1>Thank you for your interest in Fosshost</h1>
+        <p>
+            Before we proceed with your application, please confirm that you have read and understood the
+            <a href="https://docs.fosshost.org/en/home/application" target="_blank" class="fosshost-link"><b>Eligibility Criteria</b></a>.
+        </p>
+        <button class="accept" on:click={handleClick}>I accept</button>
+        <button class="cancel" on:click={() => {window.location.href = "/"}}>I don't accept</button>
+    {/if}
 </main>
 
 <style>
