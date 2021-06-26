510c0080b818058712a3173c1c0b4867fec8af47
Nikola Stojković <nikola@stojkovic.dev>
2021-06-26 03:08:31 +0200

    Added web stats page and the link to it in the footer

---
+++ src/routes/web-stats.svelte
@@ -0,0 +1,28 @@
+<script>
+    import marked from "marked";
+    import text from "../static_pages/web-stats.md";
+</script>
+
+<svelte:head>
+    <title>Fosshost - Web Statistics</title>
+</svelte:head>
+
+<main>
+    <article>
+        {@html marked(text)}
+    </article>
+</main>
+
+<style>
+    main {
+        width: auto;
+        max-width: 780px;
+        margin: 3rem auto;
+    }
+    @media (max-width: 1280px) {
+        main {
+            margin: 0.5rem auto 3rem;
+            padding: 0rem 1rem;
+        }
+    }
+</style>
