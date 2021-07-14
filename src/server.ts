cb145e02082c1dff2d79f7d7134dcd318499ceef
Seth Knights <knightss@catlin.edu>
2021-07-13 17:01:00 -0700

    Increase rate limiter for applications

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 const apiLimiter = rateLimit({
-	windowMs: 15 * 60 * 1000,
+	windowMs: 60 * 60 * 1000,
 	max: 2,
 	handler: function (req, res) {
 		res.status(429).send(req.rateLimit.resetTime);
