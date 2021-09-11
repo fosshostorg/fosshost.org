0f478076118e026d94b121166e19bfd2724b53fe
Seth Knights <knightss@catlin.edu>
2021-09-10 18:19:30 -0700

    Refactor application to only use osTicket.

--- src/routes/api/apply.ts
+++ src/routes/api/apply.ts
@@ -1,24 +1,20 @@
-const axios = require('axios');
-import { Message, SMTPClient } from 'emailjs';
-import escape from 'validator/es/lib/escape';
-import marked from 'marked';
-import { info } from '../../_utils';
+const axios = require("axios");
+import { Message, SMTPClient } from "emailjs";
+import escape from "validator/es/lib/escape";
+import marked from "marked";
+import { info } from "../../_utils";
 
+let baseURL = "https://fosshost.org";
 
-let baseURL = "https://fosshost.org"
+export async function post(req: any, res: any, next: () => void) {
+  info(`Application received: ${req.body.project.name}`);
 
-function emailFormat(body: FormResponse): any {
-    return (
-        `<h1 style="text-align: center"><img src="${baseURL}/img/HERO_IMAGE.png" alt="Fosshost Logo"/></h1>
-        <h1 style="text-align: center; margin: 1rem 0;">Thanks for joining us!<h1>
-        <h3 style="max-width: 640px; text-align: center; margin: 0 auto;">
-        This email is to confirm the successful submission of your application. To learn more about our services, visit <a href="https://fosshost.org/about">our site</a>, and to review your submitted information, see below.
-        </h3>
-        <h5 style="max-width: 640px; text-align: center; margin: 0 auto;">
-        <br>Your application is currently being reviewed by our team. Within the next 72 working hours you should receive a follow up email detailing our decision or a request for further information.
-        </h5>
-        <hr>` + marked(format(body, true))
-    )
+  const application = new Application(req.body);
+
+  await application.createOSTicket();
+  application.sendConfirmationEmail();
+
+  res.end();
 }
 
 const headers = {
@@ -56,87 +52,156 @@ const headers = {
         },
     }
 }
+class Application {
+  body: FormResponse;
+  forEmail: boolean;
 
-const format = (body: FormResponse, forEmail: boolean) => {
-    const getFormPart = (part: string): string => {
-        return (
-            escape(
-                `\n### ${headers[part].header}\n${Object.keys(body[part]).map(n => {
-                    if (n == "services" || part != "technical") {
-                        return (
-                        " - " + headers[part][n] + ": " + body[part][n]
-                        )
-                    } else {
-                        return (
-                            "\n##### " + n + " specifics:\n" + Object.keys(body[part][n]).map(p => "- " + headers[part][n][p] + ": " + body[part][n][p]).join("\n")
-                        )
-                    }
-                }).join("\n")}`
-            )
-        )
-    }
-
-    return (
-        `${forEmail ? "" : "## Fosshost Application"}\n${Object.keys(body).map(n => {
-            if (!forEmail || n != "security") {
-                return getFormPart(n);
-            }
-        }).join("\n")}`
-    )
-}
-
-const client = new SMTPClient({
-	user: process.env.EMAIL,
-	password: process.env.PASSWORD,
-	host: "smtp.office365.com",
+  client: SMTPClient = new SMTPClient({
+    user: process.env.EMAIL,
+    password: process.env.PASSWORD,
+    host: "smtp.office365.com",
     tls: {
-		ciphers: 'SSLv3',
-	},
+      ciphers: "SSLv3",
+    },
     timeout: 40000,
-});
+  });
 
-const labels = {
-    "Domain Name and DNS": "DNS",
-    "VPS": "VPS",
-    "Mirrors-as-a-service": "Maas", 
-}
+  constructor(_body: FormResponse, _forEmail: boolean = false) {
+    this.body = _body;
+    this.forEmail = _forEmail;
+  }
 
-export async function post(req: any, res: any, next: () => void) {
-    info(`Application received: ${req.body.project.name}`)
+  emailFormat = (): any => {
+    return (
+      `<h1 style="text-align: center"><img src="${baseURL}/img/HERO_IMAGE.png" alt="Fosshost Logo"/></h1>
+            <h1 style="text-align: center; margin: 1rem 0;">Thanks for joining us!<h1>
+            <h3 style="max-width: 640px; text-align: center; margin: 0 auto;">
+            This email is to confirm the successful submission of your application. To learn more about our services, visit <a href="https://fosshost.org/about">our site</a>, and to review your submitted information, see below.
+            </h3>
+            <h5 style="max-width: 640px; text-align: center; margin: 0 auto;">
+            <br>Your application is currently being reviewed by our team. Within the next 72 working hours you should receive a follow up email detailing our decision or a request for further information.
+            </h5>
+            <hr>` + this.getHTML(true)
+    );
+  };
 
-    await axios.post("https://api.github.com/repos/fosshostorg/applications/issues", 
-        {
-            title: "Application: " + req.body.project.name,
-            body: format(req.body, false),
-            labels: req.body.technical.services.map((s: string) => labels[s]),
-        },
-        {
-            auth: {
-                username: process.env.GITHUB_USER,
-                password: process.env.GITHUB_PASS
-            }  
-        },
+  getFormPart = (part: string): string => {
+    return escape(
+      `\n### ${headers[part].header}\n${Object.keys(this.body[part])
+        .map((n) => {
+          if (n == "services" || part != "technical") {
+            return " - " + headers[part][n] + ": " + this.body[part][n];
+          } else {
+            return (
+              "\n##### " +
+              n +
+              " specifics:\n" +
+              Object.keys(this.body[part][n])
+                .map(
+                  (p) =>
+                    "- " + headers[part][n][p] + ": " + this.body[part][n][p]
+                )
+                .join("\n")
+            );
+          }
+        })
+        .join("\n")}`
+    );
+  };
+
+  getMarkdown = (isEmail: boolean = false): string => {
+    return `${isEmail ? "" : "## Fosshost Application"}\n${Object.keys(
+      this.body
     )
+      .map((n) => {
+        if (!isEmail || n != "security") {
+          return this.getFormPart(n);
+        }
+      })
+      .join("\n")}`;
+  };
 
+  getHTML = (isEmail: boolean = false): string => {
+    return marked(this.getMarkdown(isEmail));
+  };
+
+  createGithubIssue = async () => {
+    await axios.post(
+      "https://api.github.com/repos/fosshostorg/applications/issues",
+      {
+        title: "Application: " + this.body.project.name,
+        body: this.getMarkdown(),
+      },
+      {
+        auth: {
+          username: process.env.GITHUB_USER,
+          password: process.env.GITHUB_PASS,
+        },
+      }
+    );
+  };
+
+  sendConfirmationEmail = () => {
     const mailOptions: Message = new Message({
         from: process.env.EMAIL,
-        to: 'support@fosshost.org',
-	      cc: req.body.personal.email,
-        subject: `Fosshost Application - ${escape(req.body.project.name)}`,
+        to: this.body.personal.email,
+	    //   cc: ,
+        subject: `Fosshost Application - ${escape(this.body.project.name)}`,
         text: "",
         attachment: [
-            { data: emailFormat(req.body), alternative: true },
+            { data: this.emailFormat(), alternative: true },
         ]
     })
 
-    client.send(mailOptions, (err, msg) => {
-        if (err) {
-            console.log(err);
-        } else if (msg) {
-            info("Email sent successfully");
-        }
-        
-    })
+    this.client.send(mailOptions, (err, msg) => {
+      if (err) {
+        console.log(err);
+      } else if (msg) {
+        info("Email sent successfully");
+      }
+    });
+  };
 
-    res.end();
+  createOSTicket = async () => {
+    const ticket = new osTicket(this);
+    await ticket.send();
+  };
+}
+
+class osTicket {
+  alert: boolean = false;
+  autorespond: boolean = false;
+  source: string = "API";
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+
+  constructor(form: Application) {
+    this.email = form.body.personal.email;
+    this.name = form.body.personal.name;
+    this.subject = `Fosshost Application - ${form.body.project.name}`;
+    this.message = `data:text/html,${form.getHTML()}`;
+  }
+
+  send = async () => {
+    await axios.post(
+      "https://support.fossho.st/api/http.php/tickets.json",
+      {
+        alert: this.alert,
+        autorespond: this.autorespond,
+        source: this.source,
+        name: this.name,
+        email: this.email,
+        subject: this.subject,
+        message: this.message,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.OSTICKET_API_KEY,
+        },
+      }
+    );
+  };
 }
