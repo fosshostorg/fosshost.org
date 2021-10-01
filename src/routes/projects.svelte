c6acdc21eb9675513ab95c801693e8a41b508b51
Thomas Markey <84930305+MrTBM@users.noreply.github.com>
2021-10-01 08:42:15 +0100

    update text
    shorten

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -10,7 +10,7 @@
 <main>
   <Header
     title="We provide a tailored hosting service to fit your needs"
-    subtitle="We provide a maintained list of projects that benefit from our service. Some of these projects may have hundreds of sub-projects that we do not list here."
+    subtitle="Below is a list of some of the projects that benefit from our service. Some of these projects may have hundreds of sub-projects that we do not list here."
     src="/img/illustration0.png"
   />
   <ProjectList />
